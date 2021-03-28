304a6b595a015be639543ce7433e4551b0d6f763
Elmando2333 <1091992414@qq.com>
2021-03-28 17:34:47 +0800

    变量命名

---
+++ demo02_01.py
@@ -0,0 +1,15 @@
+# 2021/3/28 基本语法
+
+# 全局变量使用英文大写
+SCHOOL_NAME = 'CUC'
+
+# 私有变量使用小写和一个前置下划线
+_student_name = 'Elmando'
+
+# 内置变量使用英文小写，两前导和两后导下划线
+__maker__ = 'ELmando2333'
+
+# 一般变量使用英文小写，单词间加下划线
+class_name = 'Kj-creating'
+
+
